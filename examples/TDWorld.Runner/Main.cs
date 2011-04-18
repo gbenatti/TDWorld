070c049c3fa7ecb72c718ebc72bdac12b39b7d57
gbenatti <gbenatti@gmail.com>
2011-04-18 16:37:34 -0300

    running a simple app

--- examples/TDWorld.Runner/Main.cs
+++ examples/TDWorld.Runner/Main.cs
@@ -5,14 +5,25 @@ using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
+using TDWorld.Framework;
+
 namespace TDWorld.Runner
 {
-	public class Application
-	{
+    [Register("AppDelegate")]
+    class Application : UIApplicationDelegate
+    {
+        public override void FinishedLaunching(UIApplication app)
+        {
+            // Fun begins..
+            using (var game = new GameApp())
+            {
+                game.Run();
+            }
+        }
+
 		static void Main (string[] args)
 		{
 			UIApplication.Main (args);
 		}
 	}
 }
-
